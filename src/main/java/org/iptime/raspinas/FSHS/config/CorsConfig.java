f71cef3066d49cbc71c2ae7f4983a6f22abfd832
ggm77 <shmshm86@naver.com>
2023-12-07 16:10:52 +0900

    fix : Fix hls streaming cors error

---
+++ src/main/java/org/iptime/raspinas/FSHS/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package org.iptime.raspinas.FSHS.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry){
+                corsRegistry.addMapping("/api/v1/streaming-video/**").allowedOriginPatterns("*")
+                        .allowedMethods("GET","OPTIONS");
+//                        .allowedHeaders("Origin", "Content-Type", "Accept")
+//                        .allowCredentials(true).maxAge(3600);
+            }
+        };
+    }
+}
