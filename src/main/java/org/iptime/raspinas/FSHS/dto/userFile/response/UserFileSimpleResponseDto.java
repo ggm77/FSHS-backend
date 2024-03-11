a18014e6805a32ac9dcd5b05f7219e647a296b6d
ggm77 <shm040806@gmail.com>
2024-03-11 11:58:27 +0900

    feat : Add fileName and fileExtension on userInfo

--- src/main/java/org/iptime/raspinas/FSHS/dto/userFile/response/UserFileSimpleResponseDto.java
+++ src/main/java/org/iptime/raspinas/FSHS/dto/userFile/response/UserFileSimpleResponseDto.java
@@ -11,12 +11,16 @@ import org.iptime.raspinas.FSHS.entity.userFile.UserFile;
 public class UserFileSimpleResponseDto {
     private Long id;
     private String originalFileName;
+    private String fileName;
+    private String fileExtension;
     private Long fileSize;
 
 
     public UserFileSimpleResponseDto(final UserFile userFile){
         this.id = userFile.getId();
         this.originalFileName = userFile.getOriginalFileName();
+        this.fileName = userFile.getFileName();
+        this.fileExtension = userFile.getFileExtension();
         this.fileSize = userFile.getFileSize();
     }
 }
