ce9f2f60efa4c391baed025a4a88437cf08ff9f8
ggm77 <shmshm86@naver.com>
2024-04-29 15:46:25 +0900

    feat : Add folder name update api

--- src/main/java/org/iptime/raspinas/FSHS/controller/userFolder/UserFolderController.java
+++ src/main/java/org/iptime/raspinas/FSHS/controller/userFolder/UserFolderController.java
@@ -1,16 +1,23 @@
 package org.iptime.raspinas.FSHS.controller.userFolder;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.iptime.raspinas.FSHS.dto.userFolder.request.UserFolderRequestDto;
+import org.iptime.raspinas.FSHS.dto.userFolder.response.UserFolderResponseDto;
+import org.iptime.raspinas.FSHS.dto.userInfo.response.UserInfoResponseDto;
+import org.iptime.raspinas.FSHS.entity.userFile.UserFile;
 import org.iptime.raspinas.FSHS.security.TokenProvider;
 import org.iptime.raspinas.FSHS.service.userFolder.UserFolderCreateService;
+import org.iptime.raspinas.FSHS.service.userFolder.UserFolderUpdateService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -18,6 +25,7 @@ import java.net.URI;
 public class UserFolderController {
 
     private final UserFolderCreateService userFolderCreateService;
+    private final UserFolderUpdateService userFolderUpdateService;
     private final TokenProvider tokenProvider;
 
     @PostMapping(value = "/folder")
@@ -45,5 +53,28 @@ public class UserFolderController {
         return ResponseEntity.created(location).build();
 
 
+    }
+
+    @PatchMapping(value = "/folder/{folderId}")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "폴더 업데이트 성공",
+                    content = @Content(schema = @Schema(implementation = UserFolderRequestDto.class))
+            )
+    })
+    public ResponseEntity<?> updateUserFolder(
+            @RequestHeader(value = "Authorization") final String token,
+            @PathVariable final Long folderId,
+            @RequestBody final UserFolderRequestDto userFolderRequestDto
+    ){
+
+        final String userId = tokenProvider.validate(token.substring(7));
+        final Long id = Long.parseLong(userId);
+
+        final Optional<UserFile> result = userFolderUpdateService.updateUserFolder(userFolderRequestDto, folderId, id);
+
+        return result.map(node -> ResponseEntity.ok(UserFolderResponseDto.fromEntity(node)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
---
+++ src/main/java/org/iptime/raspinas/FSHS/service/userFolder/UserFolderUpdateService.java
@@ -0,0 +1,141 @@
+package org.iptime.raspinas.FSHS.service.userFolder;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.iptime.raspinas.FSHS.dto.userFolder.request.UserFolderRequestDto;
+import org.iptime.raspinas.FSHS.entity.userFile.UserFile;
+import org.iptime.raspinas.FSHS.exception.CustomException;
+import org.iptime.raspinas.FSHS.exception.constants.ExceptionCode;
+import org.iptime.raspinas.FSHS.repository.userFile.UserFileRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserFolderUpdateService {
+
+    @Value("${user-file.directory.path}")
+    private String UserFileDirPath;
+
+    private final UserFileRepository userFileRepository;
+
+    @Transactional
+    public Optional<UserFile> updateUserFolder(
+            final UserFolderRequestDto userFolderRequestDto,
+            final Long folderId,
+            final Long userId
+    ){
+
+        //Validate the correctness of the provided file path. | 경로가 올바른지 체크(형식 체크)
+        if(!userFolderRequestDto.getPath().startsWith("/") || userFolderRequestDto.getPath().contains(".")){
+            throw new CustomException(ExceptionCode.PATH_NOT_VALID);
+        }
+
+        final String newFolderPath;
+        //파일 경로 마지막 '/' 없애기로 통일
+        if (userFolderRequestDto.getPath().endsWith("/")) {
+            newFolderPath = userFolderRequestDto.getPath().substring(0, userFolderRequestDto.getPath().length() - 1);
+        }
+        else {
+            newFolderPath = userFolderRequestDto.getPath();
+        }
+
+        final String newFolderName = newFolderPath.substring(newFolderPath.lastIndexOf("/")+1);
+        final Integer oldFolderIndex;
+        final Integer oldFolderLen;
+
+        final boolean isDuplicated;
+        try{
+            isDuplicated = userFileRepository.existsByUrlAndIsDirectory("/" + userId + newFolderPath, true);
+        } catch (DataAccessResourceFailureException ex){
+            throw new CustomException(ExceptionCode.DATABASE_DOWN);
+        } catch (Exception ex){
+            log.error("UserFolderUpdateService.updateUserFolder message:{}",ex.getMessage(),ex);
+            throw new CustomException(ExceptionCode.INTERNAL_SERVER_ERROR);
+        }
+
+        //중복 폴더 예외 처리
+        if(isDuplicated){
+            throw new CustomException(ExceptionCode.DIR_ALREADY_EXIST);
+        }
+
+
+        final UserFile folder;
+        try{
+            folder = userFileRepository.findById(folderId).get();
+        } catch (DataAccessResourceFailureException ex){
+            throw new CustomException(ExceptionCode.DATABASE_DOWN);
+        } catch (Exception ex){
+            log.error("UserFolderUpdateService.updateUserFolder message:{}",ex.getMessage(),ex);
+            throw new CustomException(ExceptionCode.INTERNAL_SERVER_ERROR);
+        }
+
+        //대상 폴더가 잘못된 경우 제외
+        if( !getParentPath(folder.getUrl().substring(userId.toString().length()+1), userId).equals(getParentPath(userFolderRequestDto.getPath(), userId))){
+            throw new CustomException(ExceptionCode.PATH_NOT_VALID);
+        }
+
+        oldFolderIndex = folder.getUrl().lastIndexOf("/") + 1;
+        oldFolderLen = folder.getUrl().substring(folder.getUrl().lastIndexOf("/")+1).length();
+
+        Path source = Paths.get(UserFileDirPath + folder.getUrl());
+        Path target = Paths.get(UserFileDirPath + "/"+userId+newFolderPath);
+
+        try {
+            Files.move(source, target);
+        } catch (IOException ex) {
+            log.error("UserFolderUpdateService.updateUserFolder message:{}",ex.getMessage(),ex);
+            throw new CustomException(ExceptionCode.FAILED_TO_MAKE_DIR);
+        }
+
+        folder.setFileName(newFolderName);
+        folder.setOriginalFileName(newFolderName);
+        folder.setUrl(newFolderPath);
+        if (folder.getChildren() != null && !folder.getChildren().isEmpty()) {
+            for (UserFile child : folder.getChildren()) {
+            updateFolderPath(child, newFolderName, oldFolderIndex, oldFolderLen);
+            }
+        }
+
+        return Optional.ofNullable(folder);
+    }
+
+    private void updateFolderPath(
+            final UserFile folder,
+            final String newName,
+            final Integer oldNameIndex,
+            final Integer oldNameLen
+    ) {
+       final String oldPath = folder.getUrl();
+       final String newPath = oldPath.substring(0,oldNameIndex)+newName+oldPath.substring(oldNameIndex+oldNameLen);
+       folder.setUrl(newPath);
+
+       if (folder.getChildren() != null && !folder.getChildren().isEmpty()) {
+           for (UserFile child : folder.getChildren()) {
+               updateFolderPath(child, newName, oldNameIndex, oldNameIndex);
+           }
+       }
+    }
+
+    private String getParentPath(final String path, final Long userId) {
+        final int lastSlashIndex = path.lastIndexOf('/');
+        final String result = lastSlashIndex > 0 ? path.substring(0, lastSlashIndex) : "";
+
+        //root폴더 예외 처리
+        if(result.equals('/')){
+            return "/" + userId;
+        }
+        else {
+            return result;
+        }
+    }
+}
