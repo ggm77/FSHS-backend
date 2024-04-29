f1062c6117cb1878afa673c12a7b4a53b3d47a49
ggm77 <shmshm86@naver.com>
2024-04-29 11:00:12 +0900

    feat : Add hls path validation

--- src/main/java/org/iptime/raspinas/FSHS/service/userFileStreaming/HlsService.java
+++ src/main/java/org/iptime/raspinas/FSHS/service/userFileStreaming/HlsService.java
@@ -1,5 +1,7 @@
 package org.iptime.raspinas.FSHS.service.userFileStreaming;
 
+import org.iptime.raspinas.FSHS.exception.CustomException;
+import org.iptime.raspinas.FSHS.exception.constants.ExceptionCode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,17 @@ public class HlsService {
             final String name,
             final String hlsFile
     ){
-        return new File(UserFileDirPath+path+"."+name+"/"+hlsFile);
+        //Validate the correctness of the provided file path. | 경로가 올바른지 체크
+        if(path.contains(".") || name.contains(".")){
+            throw new CustomException(ExceptionCode.PATH_NOT_VALID);
+        }
+
+        //Add extension based on file type | 파일 유형에 따라 확장자 추가
+        if(hlsFile.contains("_")){
+            return new File(UserFileDirPath+path+"."+name+"/"+hlsFile+".ts");
+        } else {
+            return new File(UserFileDirPath+path+"."+name+"/"+hlsFile+".m3u8");
+        }
+
     }
 }
