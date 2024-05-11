e175c1cb5ad6eb8df1d29033f6a6c9fbb0a77878
ggm77 <shm040806@gmail.com>
2024-05-11 21:52:55 +0900

    feat : Simplify streaming API usage

--- src/main/java/org/iptime/raspinas/FSHS/service/userFileStreaming/HlsService.java
+++ src/main/java/org/iptime/raspinas/FSHS/service/userFileStreaming/HlsService.java
@@ -14,20 +14,23 @@ public class HlsService {
     private String UserFileDirPath;
 
     public File getHlsFile(
-            final String path,
-            final String name,
+            final String requestPath,
             final String hlsFile
     ){
+
+        final String path = requestPath.substring(0, requestPath.lastIndexOf("."));
+        final Integer lastSlashIndex = path.lastIndexOf('/');
+
         //Validate the correctness of the provided file path. | 경로가 올바른지 체크
-        if(path.contains(".") || name.contains(".")){
+        if(path.contains(".")){
             throw new CustomException(ExceptionCode.PATH_NOT_VALID);
         }
 
         //Add extension based on file type | 파일 유형에 따라 확장자 추가
         if(hlsFile.contains("_")){
-            return new File(UserFileDirPath+path+"."+name+"/"+hlsFile+".ts");
+            return new File(UserFileDirPath+path.substring(0, lastSlashIndex+1)+"."+path.substring(lastSlashIndex+1)+"/"+hlsFile+".ts");
         } else {
-            return new File(UserFileDirPath+path+"."+name+"/"+hlsFile+".m3u8");
+            return new File(UserFileDirPath+path.substring(0, lastSlashIndex+1)+"."+path.substring(lastSlashIndex+1)+"/"+hlsFile+".m3u8");
         }
 
     }
